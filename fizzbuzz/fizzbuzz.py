ccac5e906a371e9e4ae202eac71e92d5c888d93b
juan felipe <jf@duartte.co>
2023-09-28 22:30:44 +0200

    fizzbuzz

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,20 @@
+# for some reason python needs to import a module to use user input
+import sys
+
+userNumber = int(sys.argv[1])
+
+def fizzbuzz(userNumber):
+  print("Your number is:", userNumber)
+  
+  for i in range(userNumber):
+        i += 1
+        # if the division residue is 0
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", i)
+        elif i % 3 == 0:
+            print("Fizz", i)
+        else:
+            print("Buzz", i)
+
+
+fizzbuzz(userNumber)
